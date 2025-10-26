75210585b6cf6fcdd2c7714d146c6bce733bb435
Anjali Kumari <anjalikumari822101@gmail.com>
2025-10-26 18:53:44 +0530

    Create StudentDemo.java

---
+++ Blank_Final_Field/StudentDemo.java
@@ -0,0 +1,16 @@
+ public  class StudentDemo
+{
+
+	public static void main(String[] args) 
+	{
+		
+		Student s1 = new Student();
+		System.out.println(s1.getName());
+		
+			
+		Student s2 = new Student();
+		System.out.println(s2.getName());
+		
+	}
+
+}
