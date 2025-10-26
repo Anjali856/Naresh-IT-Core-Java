7056c52e474caa9c6b72099fd26d666151bd7096
Anjali Kumari <anjalikumari822101@gmail.com>
2025-10-26 18:53:09 +0530

    Create Student.java

---
+++ Blank_Final_Field/Student.java
@@ -0,0 +1,19 @@
+ class  Student
+	
+{
+	private final String name;
+	{
+		name="Raj";
+	}
+	public Student()
+		{
+		}
+	public Student(String name)
+	{
+	}
+	public String getName()
+	{
+		return this.name;
+	}
+		
+}
